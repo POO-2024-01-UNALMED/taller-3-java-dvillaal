047e119ea04204e451e603c786928f7b987a9bd7
dvillaal <dvillaal@unal.edu.co>
2024-08-02 17:16:00 -0500

    a

--- src/main/java/taller3/televisores/Control.java
+++ src/main/java/taller3/televisores/Control.java
@@ -1,50 +1,5 @@
 package taller3.televisores;
 
 public class Control {
-	private TV tv;
-	
-	public TV getTv() {
-		return tv;
-	}
-	
-	public void setTv(TV tv) {
-		this.tv = tv;
-	}
-	
-	public void enlazar(TV televisor) {
-		tv = televisor;
-		tv.setControl(this);
-	}
-	
-	public void turnOn() {
-		tv.turnOn();
-	}
-	
-	public void turnOff() {
-		tv.turnOff();
-	}
-	
-	public void volumenUp() {
-		tv.volumenUp();
-	}
-	
-	public void volumenDown() {
-		tv.volumenDown();
-	}
-	
-	public void canalUp() {
-		tv.canalUp();
-	}
-	
-	public void canalDown() {
-		tv.canalDown();
-	}
-	
-	public void setCanal(int num) {
-		tv.setCanal(num);
-	}
-	
-	public void setVolumen(int num) {
-		tv.setVolumen(num);
-	}
+
 }
